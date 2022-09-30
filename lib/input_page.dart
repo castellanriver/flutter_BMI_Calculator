e3a63f896df9e0b315732b37af8ab0077c2cafd5
JeremyKang <castleownerkang@gmail.com>
2022-09-30 21:58:15 +0900

    BMI Calculator layout

---
+++ lib/input_page.dart
@@ -0,0 +1,88 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xffeb1555);
+const activeCardColour = Color(0xff1d1e33);
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI CALCULATOR'),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF0A0E21),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: activeCardColour,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour});
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(15.0)),
+    );
+  }
+}
