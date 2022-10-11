ee6d9305daf69c8d223ceeae0aa3102d7b9a4c2c
JeremyKang <castleownerkang@gmail.com>
2022-10-11 22:41:36 +0900

    Compleate project

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bmi_calculator/screens/input_page.dart';
 
-import 'input_page.dart';
-
-void main() => runApp(BMICalculator());
+void main() => runApp(const BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        colorScheme: const ColorScheme.light()
+            .copyWith(primary: const Color(0xFF0A0E21)),
+        scaffoldBackgroundColor: const Color(0xff090C22),
       ),
-      home: InputPage(),
+      home: const InputPage(),
     );
   }
 }
